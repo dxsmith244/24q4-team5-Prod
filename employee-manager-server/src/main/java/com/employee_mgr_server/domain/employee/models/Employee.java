69edea86d61d885246087c2db58a5918556d0067
David Smith <dxsmith244@gmail.com>
2024-12-05 09:57:09 -0500

    feat: adds new Employee object and controller for API (#29)
    * feat: adds new README for project
    
    * feat:adds new employee object and controller

---
+++ employee-manager-server/src/main/java/com/employee_mgr_server/domain/employee/models/Employee.java
@@ -0,0 +1,35 @@
+package main.java.com.employee_mgr_server.domain.employee.models;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Data 
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NonNull
+    private String firstName;
+
+    @NonNull
+    private String lastName;
+
+    @NonNull
+    private String email;
+
+    public String toString(){
+        return String.format("%d %s %s %s", id, firstName,lastName,email);
+    }
+}
