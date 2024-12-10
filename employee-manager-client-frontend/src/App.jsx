2bca22ff3fc067cbae33cae0d3aff000bd211fc9
Cogbonnia <244cogbonnia@gmail.com>
2024-12-10 09:48:08 -0500

    Add Gradient Background and Drop Shadow to App Layout (#36)
    * feat: add gradient background and drop shadow to app layout
    
    * chore: Swapped Tailwind placement
    
    * chore: changed table colors

--- employee-manager-client-frontend/src/App.jsx
+++ employee-manager-client-frontend/src/App.jsx
@@ -1,23 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import {Container} from '@mui/material';
-import {Employee} from "./pages/Employee";
-import {NoContent} from "./pages/NoContent";
-import {Update} from "./pages/Update";
-import {Add} from "./pages/Add";
-function App() {
+import { Container } from '@mui/material';
+import { Employee } from "./pages/Employee";
+import { NoContent } from "./pages/NoContent";
+import { Update } from "./pages/Update";
+import { Add } from "./pages/Add";
 
+function App() {
   return (
-    <Container maxWidth="md">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Employee />} />
-          <Route path="/update/:id" element={<Update />} />
-          <Route path="/add" element={<Add />} />
-          <Route path="*" element={<NoContent />} />
-        </Routes>
-      </BrowserRouter>
-     
-    </Container>
+    <div className="bg-gradient-to-r from-blue-500 via-purple-600 to-pink-500 min-h-screen shadow-lg">
+      <Container maxWidth="md" className="py-8">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Employee />} />
+            <Route path="/update/:id" element={<Update />} />
+            <Route path="/add" element={<Add />} />
+            <Route path="*" element={<NoContent />} />
+          </Routes>
+        </BrowserRouter>
+      </Container>
+    </div>
   );
 }
 
--- employee-manager-client-frontend/src/components/EmployeeTable.jsx
+++ employee-manager-client-frontend/src/components/EmployeeTable.jsx
@@ -1,99 +1,74 @@
-import { useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
-import {useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import * as employeeService from '../services/EmployeeService';
 import {
-    IconButton,
-    Table,
-    TableBody,
-    TableCell,
-    TableHead,
-    TableRow,
-  } from '@mui/material';
+  IconButton,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from '@mui/material';
 
 export const EmployeeTable = () => {
-    const [employees, setEmployees]= useState([]);
-    const navigate = useNavigate();
+  const [employees, setEmployees] = useState([]);
+  const navigate = useNavigate();
 
-    useEffect(()=> {
+  useEffect(() => {
+    requestDataFromApi();
+  }, []);
+
+  function requestDataFromApi() {
+    employeeService.getAllEmployees()
+      .then(res => {
+        setEmployees(res.data);
+      });
+  }
+
+  function goToUpdate(id) {
+    navigate(`/update/${id}`);
+  }
+
+  function deleteEmployee(id) {
+    employeeService.deleteEmployee(id)
+      .then(() => {
         requestDataFromApi();
-    }, []);
+      });
+  }
 
-    function requestDataFromApi(){
-        employeeService.getAllEmployees()
-        .then(res => {
-            setEmployees(res.data);
-        })
-    }
-
-    function goToUpdate(id){
-        navigate(`/update/${id}`);
-    }
-
-    function deleteEmployee (id){
-        employeeService.deleteEmployee(id)
-        .then(()=>{
-            requestDataFromApi();
-        })
-    }
-
-    return (
-        <div >
-            <Table sx={{minWidth:700}}>
-                <TableHead sx={{}}>
-                <TableRow>
-                    <TableCell>
-                        Id
-                    </TableCell>                        
-                    <TableCell>
-                        First Name
-                    </TableCell>
-                    <TableCell>
-                        Last Name
-                    </TableCell>
-                    <TableCell>
-                        Email
-                    </TableCell>
-                    <TableCell align="right">
-                        Actions
-                    </TableCell>
-                </TableRow>
-                </TableHead>
-                <TableBody>
-                    {
-                        employees.map((employee)=> {
-                            return(
-                                <TableRow
-                                    hover
-                                    key={employee.id}
-                                >
-                                    <TableCell>
-                                        {employee.id}
-                                    </TableCell>
-                                    <TableCell>
-                                        {employee.firstName}
-                                    </TableCell>
-                                    <TableCell>
-                                        {employee.lastName}
-                                    </TableCell>
-                                    <TableCell>
-                                        {employee.email}
-                                    </TableCell>
-                                    <TableCell align="right">
-                                        <IconButton component="a" onClick={()=> goToUpdate(employee.id)}>
-                                            <EditIcon />
-                                        </IconButton>
-                                        <IconButton component="a" onClick={()=> deleteEmployee(employee.id)}>
-                                            <DeleteIcon />
-                                        </IconButton>
-                                    </TableCell>
-                                </TableRow>
-                            ) 
-                        })
-                    }
-                </TableBody>
-            </Table>
-        </div>
-    )
+  return (
+    <div className="overflow-x-auto p-6 bg-white shadow-lg rounded-lg">
+      <Table className="min-w-full">
+        <TableHead className="bg-gray-100">
+          <TableRow>
+            <TableCell className="text-gray-700 font-semibold">ID</TableCell>
+            <TableCell className="text-gray-700 font-semibold">First Name</TableCell>
+            <TableCell className="text-gray-700 font-semibold">Last Name</TableCell>
+            <TableCell className="text-gray-700 font-semibold">Email</TableCell>
+            <TableCell className="text-right font-semibold">Actions</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {employees.map((employee) => (
+            <TableRow hover key={employee.id} className="transition-colors duration-200 ease-in-out">
+              <TableCell>{employee.id}</TableCell>
+              <TableCell>{employee.firstName}</TableCell>
+              <TableCell>{employee.lastName}</TableCell>
+              <TableCell>{employee.email}</TableCell>
+              <TableCell className="text-right">
+                <IconButton onClick={() => goToUpdate(employee.id)}>
+                  <EditIcon className="text-blue-600" />
+                </IconButton>
+                <IconButton onClick={() => deleteEmployee(employee.id)}>
+                  <DeleteIcon className="text-red-600" />
+                </IconButton>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
 }
