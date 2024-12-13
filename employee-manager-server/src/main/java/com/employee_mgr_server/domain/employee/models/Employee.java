5fd1226a38de933239f694b7f2f9eb68aeaf6db4
dxsmith244 <dxsmith244@gmail.com>
2024-12-13 09:19:48 -0500

    feat: deployment stuff

--- employee-manager-server/src/main/java/com/employee_mgr_server/domain/employee/models/Employee.java
+++ employee-manager-server/src/main/java/com/employee_mgr_server/domain/employee/models/Employee.java
@@ -1,40 +1,30 @@
 package com.employee_mgr_server.domain.employee.models;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Data
+@Getter
+@Setter
+@ToString
 public class Employee {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NonNull
     private String firstName;
 
-    @NonNull
     private String lastName;
 
-    @NonNull
     private String email;
 
-    @NonNull
     private String userId; // Clerk UserID
-    public String toString() {
-        return String.format("%d %s %s %s %s", id, firstName, lastName, email, userId);
-    }
 }
